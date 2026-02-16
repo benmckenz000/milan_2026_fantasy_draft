c7836e9c1d49f46c533654874a2678765386a2fb
Benjamin M <bmckenz000@citymail.cuny.edu>
2026-02-16 10:52:26 -0500

    Update medal data and leaderboard details
    Updating to add additional medal breakdown columns for silver and bronze

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ sheet = client.open("Olympic_Fantasy_Draft_2026").sheet1
 def get_medal_data():
     # Feb 15, 2026 Standings
     return {
-        "Norway": {"gold": 12, "silver": 7, "bronze": 7},
-        "South Korea": {"gold": 1, "silver": 2, "bronze": 2},
+        "Norway": {"gold": 12, "silver": 7, "bronze": 8},
+        "South Korea": {"gold": 1, "silver": 2, "bronze": 3},
         "Italy": {"gold": 8, "silver": 4, "bronze": 10},
-        "Germany": {"gold": 4, "silver": 5, "bronze": 4},
+        "Germany": {"gold": 4, "silver": 6, "bronze": 5},
         "USA": {"gold": 5, "silver": 8, "bronze": 4},
         "France": {"gold": 4, "silver": 7, "bronze": 4}
     }
@@ -33,26 +33,29 @@ def update_leaderboard():
     
     stats = get_medal_data()
     sheet.clear()
-    sheet.append_row(["Rank", "Name", "Total", "Gold", "Silver", "Bronze", "Breakdown"])
+    sheet.append_row(["Rank", "Name", "Total", "Gold", "Silver", "Bronze", "Gold Breakdown", "Silver Breakdown", "Bronze Breakdown"])
 
     final_list = []
     for name, countries in draft.items():
         g, s, b = 0, 0, 0
-        detail_list = []
+        g_details, s_details, b_details = [],[],[]
         for c in countries:
             m = stats.get(c, {"gold": 0, "silver": 0, "bronze": 0})
             g += m['gold']; s += m['silver']; b += m['bronze']
-            detail_list.append(f"{c}: {m['gold']}G")
+            g_details.append(f"{c}: {m['gold']}G")
+            s_details.append(f"{c}: {m['silver']}S")
+            b_details.append(f"{c}: {m['bronze']}B")
+            
         
         total = g + s + b
-        final_list.append([name, total, g, s, b, " | ".join(detail_list)])
+        final_list.append([name, total, g, s, b, " | ".join(g_details), "|" .join(s_details), "|" .join(b_details)])
 
     # Sort by total medals descending
     final_list.sort(key=lambda x: x[1], reverse=True)
 
     for idx, row in enumerate(final_list, start=1):
         sheet.append_row([idx] + row)
-    print("Leaderboard updated!")
+    print("Leaderboard updated")
 
 if __name__ == "__main__":
     update_leaderboard()
